5099b562a7da7ec427985f2a1bfe5c953a5ada98
williamschmidt-git <9williamschmidt@gmail.com>
2022-12-01 15:35:27 -0300

    teste

--- src/components/DrinkCarousel.jsx
+++ src/components/DrinkCarousel.jsx
@@ -1,9 +1,7 @@
-/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable no-restricted-globals */
 /* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
-// eslint-disable-next-line jsx-a11y/click-events-have-key-events
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 import React, { useState, useEffect } from 'react';
-import { Carousel } from 'react-bootstrap';
-import { useHistory } from 'react-router-dom';
 import { fetchDrinks } from '../services/helpers';
 import '../styles/carousel.css';
 
@@ -11,7 +9,7 @@ const MAX_DRINKS = 23;
 
 function DrinkCarousel() {
   const [drinks, setDrinks] = useState([]);
-  const history = useHistory();
+  const [img1, setImg1] = useState(0);
 
   const requestAPI = async () => {
     const responseAPI = await fetchDrinks();
@@ -24,21 +22,74 @@ function DrinkCarousel() {
     requestAPI();
   }, []);
 
+  const carouselLogic = (name) => {
+    const TWO = 2;
+    const onClickButtonPrevious = (img1 - TWO);
+    const onClickButtonNext = (img1 + TWO);
+    const maxLength = drinks.length;
+    if (name === 'next') {
+      if (onClickButtonNext === maxLength) {
+        return setImg1(0);
+      }
+      return setImg1(onClickButtonNext);
+    }
+    if (name === 'previous') {
+      const MINUS_TWO = -2;
+      const LAST_POSITION = 22;
+      if (img1 + MINUS_TWO === MINUS_TWO) {
+        return setImg1(LAST_POSITION);
+      }
+      return setImg1(onClickButtonPrevious);
+    }
+  };
+
   return (
-    <div className="carousel-container-all">
-      <Carousel>
-        {drinks.map((drink, index) => (
-          <Carousel.Item key={ index }>
+    <div className="carousel-container">
+      <button
+        className="btn btn-previous"
+        type="button"
+        name="previous"
+        onClick={ ({ target }) => carouselLogic(target.name) }
+      >
+        {'<'}
+      </button>
+      <div className="carousel-img-container">
+        {drinks.map((drink, index) => {
+          if (index === img1 || index === img1 + 1) {
+            return (
+              <img
+                className="img-display-view"
+                key={ index }
+                style={ { height: '50px', width: '50px' } }
+                src={ drink.strDrinkThumb }
+                name={ drink.idDrink }
+                alt="Drink"
+                data-testid={ `${index}-recomendation-card` }
+              />
+            );
+          }
+          return (
             <img
-              className="d-block w-100 img-carousel"
+              className="img-display-none"
+              key={ index }
+              style={ { height: '50px', width: '50px' } }
               src={ drink.strDrinkThumb }
+              name={ drink.idDrink }
               alt="Drink"
               data-testid={ `${index}-recomendation-card` }
               onClick={ () => history.push(`/bebidas/${drink.idDrink}`) }
             />
-          </Carousel.Item>
-        ))}
-      </Carousel>
+          );
+        })}
+      </div>
+      <button
+        className="btn btn-next"
+        type="button"
+        name="next"
+        onClick={ ({ target }) => carouselLogic(target.name) }
+      >
+        {'>'}
+      </button>
     </div>
   );
 }
